e5b8be110c9ef8a76106376b05e14c9cad7febf3
Lucas Gomes <lucaass1997rp@gmail.com>
2025-01-16 15:07:41 -0300

    :recycle: refactor: refaturando inputs, objeto para inputs criando

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import logo from './assets/fusion-logo.svg';
 const schema = z.object({
   nome: z.string().min(1, 'Nome é obrigatório'),
   email: z.string().email('Formato de e-mail inválido'),
-  telefone: z.string().trim().regex(/^[1-9]{2}[9]{0,1}[0-9]{8}$/, 'Número de telefone inválido.'),
+  telefone: z.string().trim().regex(/^[1-9]{2}[9]{0,1}[0-9]{8}$/, 'Número inválido Digite no formato 21999999999, sem espaços.'),
   cargo: z.string().min(1, 'Cargo é obrigatório'),
   linkedin: z.string().optional(),
   github: z.string().optional(),
@@ -21,12 +21,24 @@ const cargos = [
   'DevOps Engineer', 'Engenheiro de Dados', 'QA Engineer', 'Scrum Master', 'Product Owner',
 ];
 
+const inputFields = [
+  { id: 'nome', label: 'Nome completo', type: 'text', placeholder: 'Fulano', required: true },
+  { id: 'email', label: 'E-mail', type: 'email', placeholder: 'Fulano@gmail.com', required: true },
+  { id: 'telefone', label: 'Telefone', type: 'text', placeholder: '21999999999', required: true },
+  { id: 'linkedin', label: 'LinkedIn (opcional)', type: 'url', placeholder: '', required: false },
+  { id: 'github', label: 'GitHub (opcional)', type: 'url', placeholder: '', required: false },
+];
 
-type FormErrors = {
-  [key: string]: string | undefined; 
-};
+interface FormErrors {
+  nome?: string;
+  email?: string;
+  telefone?: string;
+  cargo?: string;
+  linkedin?: string;
+  github?: string;
+}
 
-const App = () => {
+const App: React.FC = () => {
   const [formData, setFormData] = useState({
     nome: '',
     email: '',
@@ -52,27 +64,16 @@ const App = () => {
 
     try {
       schema.parse(formData);
-
       setErrors({});
-
       localStorage.setItem('membro', JSON.stringify(formData));
-
       toast.success('Cadastro realizado com sucesso!');
       toast.info('Dados armazenados no localStorage.');
-
-      setFormData({
-        nome: '',
-        email: '',
-        telefone: '',
-        cargo: '',
-        linkedin: '',
-        github: '',
-      });
+      setFormData({ nome: '', email: '', telefone: '', cargo: '', linkedin: '', github: '' });
     } catch (error) {
       if (error instanceof z.ZodError) {
         const formErrors: FormErrors = {};
         error.errors.forEach((err) => {
-          formErrors[err.path[0]] = err.message;
+          formErrors[err.path[0] as keyof FormErrors] = err.message;
         });
         setErrors(formErrors);
         toast.error('Falha ao cadastrar. Verifique os dados informados.');
@@ -91,48 +92,23 @@ const App = () => {
       <img className="w-[150px] mb-4" src={logo} alt="Logo da FrontEnd Fusion" />
       <h2 className="text-white text-2xl font-bold mb-6">Cadastro de Membro</h2>
       <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 w-full max-w-md">
-        <div className="mb-4">
-          <label htmlFor="nome" className="block text-gray-700 text-sm font-bold mb-2">Nome completo</label>
-          <input
-            type="text"
-            id="nome"
-            name="nome"
-            value={formData.nome}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:border-blue-500 hover:bg-gray-100 transition duration-200"
-            placeholder='Fulano'
-          />
-          {errors.nome && <p className="text-red-500 text-xs">{errors.nome}</p>}
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:border-blue-500 hover:bg-gray-100 transition duration-200"
-            placeholder='Fulano@gmail.com'
-          />
-          {errors.email && <p className="text-red-500 text-xs">{errors.email}</p>}
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="telefone" className="block text-gray-700 text-sm font-bold mb-2">Telefone</label>
-          <input
-            type="text"
-            id="telefone"
-            name="telefone"
-            value={formData.telefone}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:border-blue-500 hover:bg-gray-100 transition duration-200"
-            placeholder='2199999999999'
-          />
-          {errors.telefone && <p className="text-red-500 text-xs">{errors.telefone}</p>}
-        </div>
-
+        {inputFields.map(({ id, label, type, placeholder}) => (
+          <div key={id} className="mb-4">
+            <label htmlFor={id} className="block text-gray-700 text-sm font-bold mb-2">
+              {label}
+            </label>
+            <input
+              type={type}
+              id={id}
+              name={id}
+              value={formData[id as keyof typeof formData]}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:border-blue-500 hover:bg-gray-100 transition duration-200"
+              placeholder={placeholder}
+            />
+            {errors[id as keyof FormErrors] && <p className="text-red-500 text-xs">{errors[id as keyof FormErrors]}</p>}
+          </div>
+        ))}
         <div className="mb-4">
           <label htmlFor="cargo" className="block text-gray-700 text-sm font-bold mb-2">Cargo pretendido</label>
           <select
@@ -145,40 +121,11 @@ const App = () => {
           >
             <option value="">Selecione um cargo</option>
             {cargos.map((cargo, index) => (
-              <option key={index} value={cargo}>
-                {cargo}
-              </option>
+              <option key={index} value={cargo}>{cargo}</option>
             ))}
           </select>
           {errors.cargo && <p className="text-red-500 text-xs">{errors.cargo}</p>}
         </div>
-
-        <div className="mb-4">
-          <label htmlFor="linkedin" className="block text-gray-700 text-sm font-bold mb-2">LinkedIn (opcional)</label>
-          <input
-            type="url"
-            id="linkedin"
-            name="linkedin"
-            value={formData.linkedin}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:border-blue-500 hover:bg-gray-100 transition duration-200"
-          />
-          {errors.linkedin && <p className="text-red-500 text-xs">{errors.linkedin}</p>}
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="github" className="block text-gray-700 text-sm font-bold mb-2">GitHub (opcional)</label>
-          <input
-            type="url"
-            id="github"
-            name="github"
-            value={formData.github}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:border-blue-500 hover:bg-gray-100 transition duration-200"
-          />
-          {errors.github && <p className="text-red-500 text-xs">{errors.github}</p>}
-        </div>
-
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-200"
@@ -190,5 +137,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
